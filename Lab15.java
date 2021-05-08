171be28e0c995b18a2a02f74e02ed7e8de13af1b
yo1996 <83825433+yo1996@users.noreply.github.com>
2021-05-08 02:27:54 -0700

    Add files via upload

---
+++ Lab15.java
@@ -0,0 +1,14 @@
+public class Lab15
+ {
+ public static void main(String[] args) {
+     
+   int a, b, temp;
+   a = 10;
+   b = 20;
+   System.out.println("Before swapping : = "+a+", "+ + b);
+   temp = a;
+   a = b;
+   b = temp;   
+  System.out.println("After swapping := "+a+", "+ + b);
+ }
+ }
