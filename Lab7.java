9c40af79877810053de5d04114e59f85b098e868
yo1996 <83825433+yo1996@users.noreply.github.com>
2021-05-08 02:26:22 -0700

    Add files via upload

---
+++ Lab7.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Lab7
+{
+ public static void main(String args[])
+{
+ int a=8;
+ System.out.println("8*1=8");
+ System.out.println("8*2=16");
+ System.out.println("8*3=24");
+ System.out.println("8*4=32");
+ System.out.println("8*5=40");
+ System.out.println("8*6=48");
+ System.out.println("8*7=56");
+ System.out.println("8*8=64");
+ System.out.println("8*9=81");
+ System.out.println("8*10=80");
+ }
+}
---
+++ Lab9.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Lab9
+{
+ public static void main(String args[])
+{
+ double a = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+ System.out.println("Expected Output="+a);
+}
+}
